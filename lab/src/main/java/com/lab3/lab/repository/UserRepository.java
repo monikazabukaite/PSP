4024a0ed2f7ca43122150be3c27a2384471c21b7
monikazabukaite <71336833+monikazabukaite@users.noreply.github.com>
2021-11-27 18:56:45 +0200

    Delete lab directory

--- lab/src/main/java/com/lab3/lab/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lab3.lab.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.lab3.lab.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
